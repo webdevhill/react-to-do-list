afa161c9f1eeefa645d414be814149964417e473
Jeffrey Hill <jdhill2142@gmail.com>
2022-06-15 11:15:06 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor (props){
+    super(props);
+
+    this.state={
+      newItem:"",
+      list: []
+    }
+  }
+
+
+ // Local storage currently not implemented
+  updateInput(key, value){
+    this.setState({
+      [key]: value
+    });
+  }
+
+  addItem () {
+    //create item with unique id
+    const newItem= {
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice()
+    };;
+
+  //copy of current list items with spread operator 
+  const list = [...this.state.list];
+
+  //add new item to list
+  list.push(newItem);
+
+  //update state with new list and reset newItem input
+  this.setState({
+    list,
+    newItem:''
+  });
+ } 
+
+ deleteItem(id){
+  //copy current list of items
+  const list = [...this.state.list];
+
+  //filter out item to be deleted
+  const updatedList = list.filter(item => item.id  !== id);
+
+  this.setState({list: updatedList})
+ }
+
+
+ render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+
+    <h1 className="app-title">MY LIST</h1>
+      
+      <div className="container">
+      <div className='container-items'
+       
+      >
+        Add an Item...
+        <br />
+        <input
+          type="text"
+          placeholder="Type item here"
+          value={this.state.newItem}
+          onChange={e => this.updateInput("newItem", e.target.value)}
+        />
+        <button
+          className="add-btn btn-floating"
+          onClick={() => this.addItem()}
+          disabled={!this.state.newItem.length}
         >
-          Learn React
-        </a>
-      </header>
+          <i className="material-icons"> + </i>
+        </button>
+        <br /> <br />
+        <ul>
+          {this.state.list.map(item => {
+            return (
+              <li key={item.id}>
+                {item.value}
+                <button className="btn btn-floating" onClick={() => this.deleteItem(item.id)}>
+                  <i class="material-icons">x</i>
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
     </div>
   );
+ }
 }
+
+
 
 export default App;
