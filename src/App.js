0b07f25f72f86c093aa27cbfdb753eedd24288b4
Jeffrey Hill <jdhill2142@gmail.com>
2022-06-15 11:24:07 -0400

    clean up

--- src/App.js
+++ src/App.js
@@ -11,8 +11,7 @@ class App extends Component {
     }
   }
 
-
- // Local storage currently not implemented
+ //Set key: value pair, Local storage currently not implemented
   updateInput(key, value){
     this.setState({
       [key]: value
@@ -20,7 +19,7 @@ class App extends Component {
   }
 
   addItem () {
-    //create item with unique id
+  //create item with unique id
     const newItem= {
       id: 1 + Math.random(),
       value: this.state.newItem.slice()
@@ -53,48 +52,42 @@ class App extends Component {
  render() {
   return (
     <div>
-
-    <h1 className="app-title">MY LIST</h1>
-      
+      <h1 className="app-title">MY LIST</h1>
       <div className="container">
-      <div className='container-items'
-       
-      >
+      <div className='container-items'>
         Add an Item...
-        <br />
-        <input
+      <br/>
+      <input
           type="text"
           placeholder="Type item here"
           value={this.state.newItem}
           onChange={e => this.updateInput("newItem", e.target.value)}
-        />
-        <button
+      />
+      <button
           className="add-btn btn-floating"
           onClick={() => this.addItem()}
           disabled={!this.state.newItem.length}
-        >
-          <i className="material-icons"> + </i>
-        </button>
-        <br /> <br />
-        <ul>
-          {this.state.list.map(item => {
-            return (
-              <li key={item.id}>
-                {item.value}
-                <button className="btn btn-floating" onClick={() => this.deleteItem(item.id)}>
-                  <i class="material-icons">x</i>
-                </button>
-              </li>
-            );
-          })}
-        </ul>
+      >
+      <i className="material-icons"> + </i>
+      </button>
+      <br/> <br/>
+      <ul>
+        {this.state.list.map(item => {
+          return (
+            <li key={item.id}>
+              {item.value}
+              <button className="btn btn-floating" onClick={() => this.deleteItem(item.id)}>
+                <i class="material-icons">x</i>
+              </button>
+            </li>
+          );
+        })}
+      </ul>
       </div>
     </div>
     </div>
   );
  }
 }
-
-
 
 export default App;
